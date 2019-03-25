19003ac36f21faef5fc8affb1b3b6197dfe8846c
Remy <drunen.v.r@gmail.com>
2019-03-25 22:35:24 +0100

    updated for working with browsersync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,57 @@
-const { src, dest, task, watch, parallel } = require("gulp");
+const { src, dest, task, watch, parallel, series } = require("gulp");
 
-//CSS related plugins
-var postcss        = require('gulp-postcss');
-var autoprefixer   = require('autoprefixer');
-var cssvars        = require('postcss-simple-vars');
-var nested         = require('postcss-nested');
+// CSS related plugins
+var postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssvars = require("postcss-simple-vars"),
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import");
 
+// Browser related plugins
+var browserSync = require("browser-sync").create();
 
 // project related variables
-var styleWatch   = ('./app/assets/styles/styles.css');
-var htmlWatch    = ('./app/index.html');
+var styleWatch = "./app/assets/styles/**/*.scss";
+var htmlWatch = "./app/index.html";
 
-var cssSRC       = ('./app/assets/styles/styles.css');
-var cssURL       = ('./app/temp/styles');
+var cssSRC = "./app/assets/styles/styles.css";
+var cssURL = "./app/temp/styles";
 
+// Tasks
+function browser_sync() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "./app/"
+    }
+  });
+}
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
 
 function html(done) {
-
-    console.log("Imagine something useful being done to your HTML here.");
-    done();
-};
+  console.log("Imagine something useful being done to your HTML here.");
+  done();
+}
 
 function css(done) {
-
-    src(cssSRC)
-    .pipe(postcss([cssvars(), nested(), autoprefixer()]))
+  src(cssSRC)
+    .pipe(postcss([cssImport(), cssvars(), nested(), autoprefixer()]))
     .pipe(dest(cssURL));
-    done();
-};
+  done();
+}
 
 function watch_files(done) {
-
-    watch(htmlWatch, html);
-    watch(styleWatch, css);
-    done();
-};
+  watch(htmlWatch, series(html, reload));
+  watch(styleWatch, series(css, reload));
+  done();
+}
 
 task("css", css);
 task("html", html);
 
 task("default", parallel(css, html));
-task("watch", watch_files);
+task("watch", parallel(browser_sync, watch_files));
